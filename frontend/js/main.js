ca76d9f23957881770bf923b62443fd863ce60fa
karatekid <mikechri@umich.edu>
2015-10-13 00:11:13 -0700

    mapping thrift object directly into knockout, trying to setup frequency, but slider not cooperating

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,27 +8,71 @@ function getDigitalInputConfig() {
     try {
       return client.getDigitalInputConfig();
     } catch(NetworkError) {
-      return "No network";
+      //Don't fail, mock currently for testing
+      //TODO: Implement better Mock setup
+      return DigitalInput();
     }
-
 }
-console.log(getDigitalInputConfig());
 
-function DigitalInModel() {
+function getClosestDigitalFreq(minFreq, maxFreq, setFreq) {
+    //Normalize
+    return maxFreq / getDigitalDivider(minFreq, maxFreq, setFreq);
+}
+function getDigitalDivider(minFreq, maxFreq, setFreq) {
+    if(setFreq < minFreq) {
+        return minFreq;
+    } else if(setFreq > maxFreq) {
+        return maxFreq;
+    }
+    return Math.round(maxFreq / setFreq);
+}
+
+//View Model mostly based off of DigitalInputConfig, with extra fields
+function DigitalInConfigVM(data) {
     var self = this;
-
+    //Thrift object
+    ko.mapping.fromJS(data, {}, self);
+    //Additional fields
     self.channels = [];
     for(var i = 0; i < 16; ++i) {
         self.channels.push({"num": i.toString()});
     }
     self.frequency = {
-        min: 0,
-        max: 2000,
-        step: 200,
-        val: 400
+        min: ko.pureComputed(function() {
+            return self.internalClkFreq()/self.divider.max();
+        }, self),
+        max: ko.pureComputed(function() {
+            return self.internalClkFreq()/self.divider.min();
+        }, self),
+        val: ko.pureComputed({
+            read: function() {
+                return self.internalClkFreq()/self.divider.val();
+            },
+            write: function(value) {
+                var min = self.internalClkFreq()/self.divider.max();
+                var max = self.internalClkFreq()/self.divider.min();
+                //TODO: divider, not actually getting set when called from
+                //slider, but works fine if called from console, no idea what
+                //the issue is.
+                self.divider.val(getDigitalDivider(min, max , value));
+            },
+            owner: self
+        }),
+        step: 1
+    }
+    self.textS = ko.observable("textS");
+}
+var mapping = {
+    create: function(options) {
+        return new DigitalInConfigVM(options.data);
     }
 }
-
-ko.applyBindings(new DigitalInModel());
+//A global instance of the digitalIn configuration
+var digitalInConfig = ko.mapping.fromJS(getDigitalInputConfig(), mapping);
+ko.applyBindings(digitalInConfig, document.getElementById('digitalin'));
 
 $("#digital-freq-select").slider();
+$("#digital-freq-select").on("slide", function(slideEvt) {
+    console.log("sliding");
+    digitalInConfig.frequency.val(slideEvt.value);
+});
---
+++ resources/links.md
@@ -0,0 +1,2 @@
+[Knockout JS
+Tips](https://www.airpair.com/knockout/posts/top-10-mistakes-knockoutjs)
