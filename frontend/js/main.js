251bfa8ff02555c65cbbc94cf68656ade327af83
karatekid <mikechri@umich.edu>
2015-10-14 22:19:57 -0700

    using components

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -58,11 +58,43 @@ function DigitalInConfigVM(data) {
         step: 1
     }
 }
-var mapping = {
+var DigitalInMapping = {
     create: function(options) {
         return new DigitalInConfigVM(options.data);
     }
 }
+
+// Must register components before applying bindings
+ko.components.register('crInput', {
+    viewModel: function(params) {
+        var self  = this;
+        self.min  = (params && params.data && params.data.min) || 0;
+        self.max  = (params && params.data && params.data.max) || 0;
+        self.val  = (params && params.data && params.data.val) || 0;
+        self.name = (params && params.name) || 0;
+    },
+    template:
+        "<label data-bind=\"text: name + '[' + min() + ', ' + max() + ']: '\"></label>" +
+        '<input type="text" class="form-control"' +
+                'data-bind="value: val">'
+});
+ko.components.register('setInput', {
+    viewModel: function(params) {
+        var self = this;
+        self.roptions = (params && params.data && params.data.options) || 0;
+        self.val = (params && params.data && params.data.val) || 0;
+        self.dict = (params && params.dict) || 0;
+        self.name = (params && params.name) || 0;
+    },
+    template:
+        "<label data-bind='text: name'></label>" +
+        "<select class='form-control' " +
+                 "data-bind='options: roptions, " +
+                           "optionsText: function(key) {" +
+                                "return dict[key];}, " +
+                           "value: val'></select>"
+});
+
 //A global instance of the digitalIn configuration
-var digitalInConfig = ko.mapping.fromJS(getDigitalInputConfig(), mapping);
+var digitalInConfig = ko.mapping.fromJS(getDigitalInputConfig(), DigitalInMapping);
 ko.applyBindings(digitalInConfig, document.getElementById('digitalin'));
