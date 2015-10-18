7a9609dc2e4896801bbc6e1bb7ec07f1d3f9cae8
karatekid <mikechri@umich.edu>
2015-10-18 14:06:58 -0700

    got a function working to accept data and graph it

--- frontend/js/app/graph.js
+++ frontend/js/app/graph.js
@@ -38,7 +38,6 @@ define(["lib/d3.min"], function(d3) {
     line.interpolate('step-after');
 
     // Generate fake data
-    var stepper = [100, 300, 10, 20, 200];
     var steppers = [
         [100, 300, 10, 20, 200, 70],
         [100, 200, 100, 150, 150],
@@ -60,16 +59,30 @@ define(["lib/d3.min"], function(d3) {
     for(var i = 0; i < steppers.length; ++i) {
         allData.push(genFakeData(steppers[i]));
     }
+    var updateSteppers = [
+        [300, 100, 30, 130, 140],
+        [50, 150, 90, 160, 150],
+        [100, 400, 100, 100],
+        [120, 180, 400],
+        [300, 100, 30, 130, 140],
+        [50, 150, 90, 160, 150],
+        [100, 400, 100, 100],
+        [120, 180, 400],
+        [300, 100, 30, 130, 140],
+        [50, 150, 90, 160, 150],
+        [100, 400, 100, 100],
+        [120, 180, 400],
+        [300, 100, 30, 130, 140],
+        [50, 150, 90, 160, 150],
+        [100, 400, 100, 100],
+        [120, 180, 400],
+        ];
+    var updateData = [];
+    for(var i = 0; i < updateSteppers.length; ++i) {
+        updateData.push(genFakeData(updateSteppers[i]));
+    }
 
-    // Use fake data
-    x.domain([
-            d3.min(allData, function(c) { return d3.min(c, function(d, i) { return i; }); }),
-            d3.max(allData, function(c) { return d3.max(c, function(d, i) { return i; }); })
-            ]);
-    y.domain([
-            d3.min(allData, function(c) { return d3.min(c, function(d) { return d; }); }),
-            d3.max(allData, function(c) { return d3.max(c, function(d) { return d; }); })
-            ]);
+
     // SVG Creation / update
     var svg = d3.select("#digitalin-graph-container").append("svg")
         .attr("width", width + margin.left + margin.right)
@@ -82,14 +95,37 @@ define(["lib/d3.min"], function(d3) {
     // Lines
     var lineContainer = svg.append("g")
         .attr("id", "lineContainer");
-    var lines = lineContainer.selectAll("g").data(allData);
-    lines.enter()
-        .append("g")
-        .attr("height", lineHeight)
-        .attr("transform", function(d, i) { return "translate(" + margin.left + "," + (margin.top + i*(lineHeight + linePadding)) + ")";})
-        .attr("id", function(d, i) { return "line" + i; })
-        .append("path")
-        .attr("class", "line")
-        .attr("d", line)
-        .attr("stroke", function(d, i) { return colors(i); });
+    var updateLines = function(data) {
+        // Setup domain
+        x.domain([
+                d3.min(data, function(c) { return d3.min(c, function(d, i) { return i; }); }),
+                d3.max(data, function(c) { return d3.max(c, function(d, i) { return i; }); })
+                ]);
+        y.domain([
+                d3.min(data, function(c) { return d3.min(c, function(d) { return d; }); }),
+                d3.max(data, function(c) { return d3.max(c, function(d) { return d; }); })
+                ]);
+        // Get Lines
+        var lines = lineContainer.selectAll("g").data(data);
+        // Update only
+        // Enter only
+        lines.enter()
+            .append("g")
+            .attr("height", lineHeight)
+            .attr("transform", function(d, i) { return "translate(" + margin.left + "," + (margin.top + i*(lineHeight + linePadding)) + ")";})
+            .attr("id", function(d, i) { return "line" + i; })
+            .append("path")
+            .attr("class", "line")
+            .attr("stroke", function(d, i) { return colors(i); });
+        // Update & enter
+        lines.select("path")
+            .attr("d", line);
+        // Exit
+        lines.exit().remove();
+    };
+    updateLines(allData);
+    return {
+        updateLines: updateLines,
+        updateData: updateData
+    };
 });
