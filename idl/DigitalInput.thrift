dece3750b9eb29b2f81dc42a1bb8cf2abe84a1ae
karatekid <mikechri@umich.edu>
2015-11-12 00:05:01 -0800

    Added a better mock digital input to allow for better testing without device

--- idl/DigitalInput.thrift
+++ idl/DigitalInput.thrift
@@ -15,3 +15,49 @@ struct DigitalInput {
     // Instrument Information
     8: optional double         internalClkFreq
 }
+
+const DigitalInput MockDigitalInput = {
+    "divider": {
+        "min": 1,
+        "val": 1,
+        "max": -2147483648,
+    },
+    "bufferSize": {
+        "min": 1,
+        "val": 4096,
+        "max": 4096,
+    },
+    "acquisitionMode": {
+        "val": 0,
+        "options": [0, 1]
+    },
+    "triggerSource": {
+        "val": 0,
+        "options": [0, 1, 2, 3]
+    },
+    "triggerPosition": {
+        "min": 0,
+        "val": 0,
+        "max": -2147483648,
+    },
+    "triggerAutoTimeout": {
+        "min": 0,
+        "val": 0,
+        "max": 53.477376,
+    },
+    "trigger": {
+        "val": {
+            "LevelLow":  [],
+            "LevelHigh": [],
+            "EdgeRise":  [],
+            "EdgeFall":  []
+        },
+        "options": {
+            "LevelLow":  [0,1,2,3],
+            "LevelHigh": [0,1,2,3],
+            "EdgeRise":  [0,1,2,3],
+            "EdgeFall":  [0,1,2,3]
+        }
+    },
+    "internalClkFreq": 100000000.0
+}
